9e27867fe34d27e2265361aa871539854a0df8bc
Laksh <laksharora28@gmail.com>
2016-04-03 09:49:53 +0530

    wol query working

--- wol/wol.py
+++ wol/wol.py
@@ -1,6 +1,11 @@
 import requests
 import sys
 import argparse
+import wolframalpha as wa
+import os
 
-# step 1
-# python wol.py "integrate sinx from zero to pi"
+# wapp_id should be in your environment variable
+app_id = os.environ['wapp_id']
+client = wa.Client(app_id)
+res = client.query(sys.argv[1])
+print res.pods[0].text
