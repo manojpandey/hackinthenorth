d4de1f78e945c83bebaaf581f3f702f19904a219
Manoj Pandey <manojpandey1996@gmail.com>
2016-04-03 10:31:32 +0530

    author update

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
   packages = ['wol'], # this must be the same as the name above
   version = '0.1',
   description = 'instant query from wolframalpha',
-  author = 'Manoj Pandey',
-  author_email = 'manojpandey1996@gmail.com',
+  author = 'Manoj Pandey, Laksh Arora, Akul Mehra',
+  author_email = 'manojpandey1996@gmail.com, laksharora28@gmail,com, mehra.akul@gmail.com',
   url = 'https://github.com/manojpandey/hackinthenorth/tree/wol', # use the URL to the github repo
   download_url = 'https://github.com/manojpandey/hackinthenorth/archive/wol.zip', # I'll explain this in a second
   keywords = ['wolframalpha', 'command-line', 'terminal', 'query'], # arbitrary keywords
