0e0515ba3c76bc3325be40bff2137a85894880fb
Manoj Pandey <manojpandey1996@gmail.com>
2016-04-03 10:08:52 +0530

    final

--- wol/wol.py
+++ wol/wol.py
@@ -4,8 +4,12 @@ import argparse
 import wolframalpha as wa
 import os
 
-# wapp_id should be in your environment variable
-app_id = os.environ['wapp_id']
-client = wa.Client(app_id)
-res = client.query(sys.argv[1])
-print res.pods[0].text
+def wol_cli():
+	# wapp_id should be in your environment variable
+	app_id = os.environ['wapp_id']
+	client = wa.Client(app_id)
+	res = client.query(sys.argv[1])
+	print res.pods[0].text
+
+if __name__ == '__main__':
+	wol_cli()
